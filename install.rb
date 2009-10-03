4df537294f4c5c8dc2868c093147e4fe5cf98f2c
Mike Dvorkin <mike@dvorkin.net>
2009-10-03 14:33:09 -0700

    Added post installation instructions

--- install.rb
+++ install.rb
@@ -1,2 +1,16 @@
 # Install hook code here
-# TODO: install [acts-as-taggable-on] plugin.
+puts <<-EOS
+The Tags adds tagging support to all major Fat Free CRM models. To search by tag enter its
+name prefixed by # character. For example:
+
+  Search: hello #world
+
+In this example "hello" is regular search string, and "world" is a tag.
+
+The Tags plugin depends on [acts-as-taggable-on] plugin which must be installed as follows:
+
+  $ ./script/plugin install git://github.com/mbleigh/acts-as-taggable-on.git
+  $ ./script/generate acts_as_taggable_on_migration
+  $ rake db:migrate
+
+EOS
